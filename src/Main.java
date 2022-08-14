183ffa57f7d2d710d53b972ec3068e8adef03ecc
Алина Арасланова <aaraslanova.alina@yandex.ru>
2022-08-14 19:25:34 +0300

    Сделанное дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+        int i = 0;
+        while (i < 10) {
+            i = i + 1;
+            System.out.print(i + " ");
+        }
+        System.out.println(" ");
+        for (i = 10; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println(" ");
+        //Задание 2
+        int a = 5;
+        for (a = 5; a < 31; a = a + 7) {
+            System.out.println("сегодня пятница " + a + " число " + " нужно подготовить отчёт");
+        }
+        //Задание 3
+        int year1 = 1822, year2= 2122;
+        for (int year = 0; year <= year2; year = year + 79) {
+            if (year >= year1) {
+
+
+                System.out.println(year);
+
+            }
+        }
+
     }
 }
+
+
